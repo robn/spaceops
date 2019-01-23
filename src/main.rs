8baa40df33623d82c727182442148afd9525bd9e
Rob N ★ <robn@robn.io>
2019-01-23 23:59:32 +1100

    Cleanup vague rantings

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,6 @@ fn main() {
     let mut win = three::Window::new("🚀 spaceops 🛰️");
 
     win.scene.background = three::Background::Color(0x000000);
-    /*
-    let backtex = win.factory.load_texture("/Users/robn/Downloads/22687644_702004036675566_4623325051301079301_n.jpg");
-    win.scene.background = three::Background::Texture(backtex);
-    */
 
     let camera = win.factory.perspective_camera(60.0, 1.0 .. 100.0);
     camera.look_at([0.0, 0.0, 5.0], [0.0, 0.0, 0.0], None);
@@ -96,99 +92,23 @@ fn release_mouse(win: &three::Window) {
 fn make_objects(factory: &mut three::Factory, n: u8) -> Vec<three::Mesh> {
     let geom = factory.upload_geometry(three::Geometry::cuboid(1.0, 1.0, 1.0));
 
-    //println!("{:#?}", geom.base.vertices);
-    /*
-    geom.tex_coords = vec!(
-        [0.0,0.0].into(),
-        [0.0,1.0].into(),
-        [1.0,1.0].into(),
-        [1.0,0.0].into(),
-
-        [0.0,0.0].into(),
-        [0.0,1.0].into(),
-        [1.0,1.0].into(),
-        [1.0,0.0].into(),
-
-        [0.0,0.0].into(),
-        [0.0,1.0].into(),
-        [1.0,1.0].into(),
-        [1.0,0.0].into(),
-
-        [0.0,0.0].into(),
-        [0.0,1.0].into(),
-        [1.0,1.0].into(),
-        [1.0,0.0].into(),
-
-        [0.0,0.0].into(),
-        [0.0,1.0].into(),
-        [1.0,1.0].into(),
-        [1.0,0.0].into(),
-
-        [0.0,0.0].into(),
-        [0.0,1.0].into(),
-        [1.0,1.0].into(),
-        [1.0,0.0].into(),
-    );
-    */
-
-    /*
-    let mat = three::material::Lambert {
-        color: 0x33aa33,
-        flat: false,
-    };
-    */
-    /*
-    let mat = three::material::Phong {
-        color: 0x33aa33,
-        glossiness: 10.0,
-    };
-    */
-
-    /*
-    let map = factory.load_texture("/Users/robn/Downloads/roll_safe.jpg");
-    let mat = three::material::Basic {
-        color: 0xffffff,
-        map: Some(map),
-    };
-    */
-
-    (0u8..n).map(|_|
-                 factory.create_instanced_mesh(&geom,
-                                               three::material::Lambert {
-                                                   color: {
-                                                       let c = random_color::RandomColor::new().to_rgb_array();
-                                                        c[0] << 16 | c[1] << 8 | c[2]
-                                                   },
-                                                   flat: false,
-                                               })
-                 ).collect()
-
-    /*
-    let group = factory.group();
-
-    let off = 1.6 * ((n as f32) - 1.0) * 0.5;
-    for x in 0u8..n {
-        for y in 0u8..n {
-            let m = factory.mesh_instance(&mesh);
-            m.set_position([f32::from(x) * 1.6 - off, f32::from(y) * 1.6 - off, 0.0]);
-            group.add(&m);
-        }
-    }
-
-    group
-    */
+    (0u8..n).map(|_| {
+        let mat = three::material::Lambert {
+            color: {
+                let c = random_color::RandomColor::new().to_rgb_array();
+                c[0] << 16 | c[1] << 8 | c[2]
+            },
+            flat: false,
+        };
+        factory.create_instanced_mesh(&geom, mat)
+    }).collect()
 }
 
 fn make_object_positions(n: u8) -> Vec<[f32; 3]> {
     let w: u8 = f32::from(n).sqrt().ceil() as u8;
     let off = GAP * ((w as f32) - 1.0) * 0.5;
-    let v = (0u8..n).fold(vec!(), |mut vv, nn| {
+    (0u8..n).fold(vec!(), |mut vv, nn| {
         vv.push([f32::from(nn % w) * GAP - off, f32::from(nn / w) * GAP - off, 0.0]);
         vv
-    });
-    //let nx: u8 = 0;
-    //let nx: u8 = n - ny * ny;
-    //println!("{}: {} {}", n, nx, ny);
-    println!("{:?}", v);
-    v
+    })
 }
